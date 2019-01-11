23db486cdbda8c9e23c092ccd35fe0e14cda8295
odedrak <odedrak@hotmail.com>
2019-01-11 18:00:48 +0100

    Sección 13 terminada

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 
 
 // Para ejecutar funcion de JQuery que inicializa los componentes del template
 declare function init_plugins();
+// Google
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -12,14 +17,60 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router) { }
+  email: string;
+  recuerdame: boolean = false;
+
+  auth2: any;
+
+
+  constructor( public router: Router, public _usuarioService: UsuarioService) { }
+
+ 
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+
+    if (this.email.length > 0 ) {
+      this.recuerdame = true;
+    }
   }
 
-  entrar() {
-    this.router.navigate(['/dashboard']);
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '131170900646-guf4rf2ippbls9m06ava7ilv17f7mumo.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin( document.getElementById('btnGoogle'));
+
+    });
+  }
+
+  attachSignin( element ) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      // const profile = googleUser.getBasicProfile();
+      const token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle(token).subscribe( correcto => this.router.navigate(['/dashboard']));
+
+      // Si falla al visualizarse la pantalla tras el login de google, usar redireccion manual
+      // this._usuarioService.loginGoogle(token).subscribe( correcto => window.location.href = '#/dashboard'));
+    });
+  }
+
+  entrar(forma: NgForm) {
+    if (forma.invalid) {
+      return;
+    }
+
+    const usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recuerdame).subscribe( correcto => this.router.navigate(['/dashboard']));
   }
 
 }
